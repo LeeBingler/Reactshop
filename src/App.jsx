27d8abeae5cd1525594fe23603450ce353cd6fd6
LeeBingler <binglerlee@gmail.com>
2023-07-07 10:54:28 +0200

    feat: add react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+
 function App() {
 
   return (
-    <>
-      yeah !
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 )
