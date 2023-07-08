f2dcc7996d1bb6436cae216a9de435c7c5b2c502
LeeBingler <binglerlee@gmail.com>
2023-07-08 14:56:07 +0200

    feat: add react-loading

--- src/Pages/LoadingScreen.jsx
+++ src/Pages/LoadingScreen.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import ReactLoading from 'react-loading';
 
 export default function LoadingScreen() {
+
   return (
-    <div>
-        LoadingScreen
-    </div>
+    <section className='flex w-screen h-screen justify-center items-center'>
+        <ReactLoading type="bars" color="#60a5fa"
+            height={200} width={100} />
+    </section>
   )
 }
