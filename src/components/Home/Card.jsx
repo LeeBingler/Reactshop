4fc3f622a2c5b3dbbbae8a5016a7d85f5cedeba1
LeeBingler <binglerlee@gmail.com>
2023-07-09 20:19:26 +0200

    feat: add style for the card

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -5,11 +5,11 @@ export default function Card({ item }) {
     return (
         <div className='w-48 bg-white border border-black rounded p-5 m-5 hover:shadow-2xl hover:scale-[1.03] transition-all ease-linear duration-200'>
             <div>
-                <p> {item.rating.rate} / 5 ⭐ </p>
+                <p className='text-right pb-5'> ⭐ {item.rating.rate} </p>
                 <img src={item.image} alt={item.title} />
             </div>
+            <h1 className='py-2 overflow-hidden text-ellipsis whitespace-nowrap text-gray-600'> {item.title} </h1>
             <div className='flex flex-col justify-center items-center'>
-                <h1 className='whitespace-nowrap'> {item.title} </h1>
                 <p> ${item.price} </p>
                 <button className='btn-navBar'> Add to Cart </button>
             </div>
