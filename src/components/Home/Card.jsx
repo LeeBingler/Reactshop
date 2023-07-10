f9c56969b4598a7f579708c16736fa5514841534
LeeBingler <binglerlee@gmail.com>
2023-07-10 14:51:55 +0200

    feat: add style card

--- src/components/Home/Card.jsx
+++ src/components/Home/Card.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 export default function Card({ item }) {
 
     return (
-        <div className='flex flex-col justify-center w-48 bg-white border border-black rounded p-4 m-4 hover:shadow-2xl hover:scale-[1.03] transition-all ease-linear duration-200'>
-            <div>
-                <p className='text-right pb-5'> ⭐ {item.rating.rate} </p>
-                <img src={item.image} alt={item.title} />
+        <div className='flex flex-col justify-center w-48 h-[360px] bg-white border border-black rounded px-4 m-4 hover:shadow-2xl hover:scale-[1.03] transition-all ease-linear duration-200'>
+            <div className='flex flex-col justify-center items-center'>
+                <p className='text-right pb-5 w-full'> ⭐ {item.rating.rate} </p>
+                <img className='h-40 w-auto' src={item.image} alt={item.title} />
             </div>
             <h1 className='py-2 overflow-hidden text-ellipsis whitespace-nowrap text-gray-600'> {item.title} </h1>
             <div className='flex flex-col justify-center items-center'>
