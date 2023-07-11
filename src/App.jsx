efcea79a15a6f16af14f608e36e3e102d9c3e85d
LeeBingler <binglerlee@gmail.com>
2023-07-11 15:27:17 +0200

    feat: add the cart provider to the app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import ItemsProvider from './components/Provider/ItemsProvider';
+import CartProvider from './components/Provider/CartProvider';
 import NoPage from './Pages/NoPage';
 import Home from './Pages/Home';
 import About from './Pages/About';
@@ -12,14 +13,16 @@ function App() {
 
   return (
     <ItemsProvider>
-      <NavBar />
-      <Routes>
-        <Route index path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path='*' element={<NoPage />} />
-      </Routes>
+      <CartProvider>
+        <NavBar />
+        <Routes>
+          <Route index path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path='*' element={<NoPage />} />
+        </Routes>
+      </CartProvider>
     </ItemsProvider>
   )
 }
--- src/components/Provider/CartProvider.jsx
+++ src/components/Provider/CartProvider.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 
 export const CartContext = React.createContext();
 export const AddCartContext = React.createContext();
