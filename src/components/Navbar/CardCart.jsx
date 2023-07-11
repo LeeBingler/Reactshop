e87407ba5b0f0168586832d6b6796f5539593ca7
LeeBingler <binglerlee@gmail.com>
2023-07-11 16:14:39 +0200

    fix: use onClickhandler instead of a hook

--- src/components/Navbar/CardCart.jsx
+++ src/components/Navbar/CardCart.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { useRemoveItemCart } from '../Provider/CartProvider';
 
 export default function CardCart({ item }) {
-    const onClickHandle = useRemoveItemCart();
+    const onClickHandler = useRemoveItemCart();
 
     return (
-        <div key={item.id}>
+        <div>
             <img className='w-10' src={item.image} alt={item.title} />
             <h1> {item.title} </h1>
             <p> ${item.price} </p>
             <p> x{item.number}</p>
             <button
             className='rounded text-white bg-black p-1'
-            onClick={() => {useRemoveItemCart(item)}}
+            onClick={() => {onClickHandler(item)}}
             >
                 Remove
             </button>
