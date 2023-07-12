ad2bf30a349fecff2bc633bca7639c27dbd08de2
LeeBingler <binglerlee@gmail.com>
2023-07-12 12:45:22 +0200

    UI: reverse the navbar info when md breakpoint

--- src/components/Navbar/MenuNav.jsx
+++ src/components/Navbar/MenuNav.jsx
@@ -8,17 +8,17 @@ export default function MenuNav({ showMenu }) {
     return (
         <div
         className={`menu-navbar border-r border-black ${isHidden}
-            md:transition-none md:absolute md:top-1 md:h-12 md:flex-row md:border-none
-            md:overflow-y-hidden md:w-[72%] md:left-40 md:overflow-x-visible
+            md:transition-none md:absolute md:top-1 md:h-12 md:flex-row-reverse md:border-none
+            md:overflow-y-hidden md:w-[72%] md:right-16 md:overflow-x-visible
             md:justify-between `}>
             <div className='flex items-center'>
                 <input
                 className='block border p-2 m-4 border-black
-                    md:py-1 md:w-48 md:text-lg md:h-8 md:mr-2 md:ml-5'
+                    md:py-1 md:w-48 md:text-lg md:h-8 md:mr-0 md:ml-2'
                 placeholder='Type to search..'>
                 </input>
                 <button className='text-2xl border-2 p-2 m-4 rounded hover:text-blue-400
-                    md:text-xl md:p-1'>
+                    md:text-xl md:p-1 md:mr-1'>
                     <i className='bx bx-search-alt'></i>
                 </button>
             </div>
