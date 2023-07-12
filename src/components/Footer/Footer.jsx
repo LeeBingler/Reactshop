b525a962329f28042633420bed492685f740a233
LeeBingler <binglerlee@gmail.com>
2023-07-12 19:43:10 +0200

    feat: add signature

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,10 @@ export default function Footer() {
     <>
         <hr className='border-black mx-14 mt-20'/>
         <div className='pt-4'>
-
             <InfoFooter classProps={'flex flex-col-reverse justify-between h-16'}/>
+            <p className='text-center pt-4 text-[15px] text-gray-700'>
+              Website made by <a className='underline' href="https://github.com/LeeBingler"> Lee Bingler</a> with ❤️
+            </p>
         </div>
     </>
   )
