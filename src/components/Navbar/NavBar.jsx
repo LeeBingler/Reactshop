be0f89ac46a44e235635a3e4182bdabc19d9f275
LeeBingler <binglerlee@gmail.com>
2023-07-13 17:31:21 +0200

    fix: the navbar dont infinite sroll

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -50,7 +50,7 @@ export default function NavBar() {
     const menuActive = showMenu ? 'text-blue-400' : 'text-black';
 
     return (
-        <header className='sticky top-0 bg-white z-50 h-14'>
+        <header className='fixed w-full top-0 bg-white z-50 h-14'>
             <nav className='navbar'>
                 <button className={`md:hidden btn-navBar ml-3 ${menuActive}`} onClick={handleClickMenu}>
                     <i className='bx bx-menu float-none inline text-3xl'></i>
