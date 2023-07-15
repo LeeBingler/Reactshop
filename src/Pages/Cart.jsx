59c9a5cd2bce67c295fef0ccf7cb8e1024c924bb
LeeBingler <binglerlee@gmail.com>
2023-07-15 17:04:02 +0200

    feat: add button to pay

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -8,14 +8,23 @@ export default function Cart() {
 
     return (
       <section className='pt-20 bg-gray-200'>
-        <div className='bg-white max-w-7xl'>
+        <div className='bg-white max-w-7xl border border-black'>
           <h1 className='font-logo text-6xl'> Your cart </h1>
           <hr />
           {ItemCart.map((item) => <CartCard item={item} /> )}
           <hr />
-          <p className='text-right'>
+          <p className='text-right text-2xl'>
             Total ({ItemCart.length} {ItemCart.length > 1 ? 'articles' : 'article'}): {totalPrice}$
           </p>
+        </div>
+        <div className='flex flex-col items-center border border-black bg-white m-4 p-2'>
+          <p className='text-right text-xl'>
+            Total ({ItemCart.length} {ItemCart.length > 1 ? 'articles' : 'article'}): {totalPrice}$
+          </p>
+          <button
+          className="rounded border border-black p-2 m-2 bg-blue-400 text-white whitespace-nowrap">
+            Proceed to Checkout
+          </button>
         </div>
       </section>
     )
