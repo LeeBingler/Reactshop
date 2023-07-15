87ee9e53e2d80f7e6e99cd26c57def2a8ff4fc55
LeeBingler <binglerlee@gmail.com>
2023-07-15 18:13:52 +0200

    feat: add btn add and remove on cart card page

--- src/components/Cart/CartCard.jsx
+++ src/components/Cart/CartCard.jsx
@@ -1,17 +1,34 @@
 import React from 'react'
+import { useAddItemCart, useRemoveItemCart } from '../Provider/CartProvider'
 
 export default function CartCard({ item }) {
+    const handleClickRemove = useRemoveItemCart();
+    const handleClickAdd = useAddItemCart();
 
     return (
-        <div className='mt-8 flex'>
-            <img className='h-20 px-6' src={item.image} alt={item.title} />
+        <div className='mt-10 lg:mt-14 flex'>
+            <img className='h-20 px-6 lg:h-28' src={item.image} alt={item.title} />
             <div className='flex flex-col w-full max-w-4xl'>
                 <div className='flex justify-between'>
-                    <h1 className='text-xl'> {item.title} </h1>
+                    <h1 className='text-xl lg:text-2xl'> {item.title} </h1>
                     <p className='pr-4'> x{item.number}</p>
                 </div>
-                <p> ${item.price} </p>
-                <p className='text-gray-500 text-sm pt-2'> {item.description} </p>
+                <p className='lg:text-xl'> ${item.price} </p>
+                <p className='text-gray-500 text-sm pt-2 lg:text-base'> {item.description} </p>
+                <div className='flex gap-7 mt-4'>
+                    <button
+                    className='border rounded border-black p-1 m-1'
+                    onClick={() => handleClickRemove(item, item.number - 1)}
+                    >
+                        Remove One
+                    </button>
+                    <button
+                    className='rounded text-white bg-black p-1 m-1'
+                    onClick={() => handleClickAdd(item, item.number + 1)}
+                    >
+                        Add One
+                    </button>
+                </div>
             </div>
         </div>
     )
