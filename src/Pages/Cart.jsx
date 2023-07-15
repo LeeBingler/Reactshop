3a97117bb45fd6cad37388e64a7c09ae267c1d24
LeeBingler <binglerlee@gmail.com>
2023-07-15 16:56:02 +0200

    feat: add construction of the Cart

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { useCart } from '../components/Provider/CartProvider';
+import { useCart, useTotalPriceCart } from '../components/Provider/CartProvider';
 import CartCard from '../components/Cart/CartCard';
 
 export default function Cart() {
   const ItemCart = useCart();
+  const totalPrice = useTotalPriceCart();
 
     return (
-      <div className='pt-20'>
-        {ItemCart.map((item) => <CartCard item={item} /> )}
-      </div>
+      <section className='pt-20 bg-gray-200'>
+        <div className='bg-white max-w-7xl'>
+          <h1 className='font-logo text-6xl'> Your cart </h1>
+          <hr />
+          {ItemCart.map((item) => <CartCard item={item} /> )}
+          <hr />
+          <p className='text-right'>
+            Total ({ItemCart.length} {ItemCart.length > 1 ? 'articles' : 'article'}): {totalPrice}$
+          </p>
+        </div>
+      </section>
     )
 };
