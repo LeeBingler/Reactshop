3469317d919f76d1a31692e784434149e1995820
LeeBingler <binglerlee@gmail.com>
2023-07-15 17:09:06 +0200

    UI: style for the cart page

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -9,10 +9,10 @@ export default function Cart() {
     return (
       <section className='pt-20 bg-gray-200'>
         <div className='bg-white max-w-7xl border border-black'>
-          <h1 className='font-logo text-6xl'> Your cart </h1>
-          <hr />
+          <h1 className='font-logo text-6xl mb-4 ml-4 mt-4'> Your cart : </h1>
+          <hr className='mx-14 mt-8'/>
           {ItemCart.map((item) => <CartCard item={item} /> )}
-          <hr />
+          <hr className='w-full mx-20' />
           <p className='text-right text-2xl'>
             Total ({ItemCart.length} {ItemCart.length > 1 ? 'articles' : 'article'}): {totalPrice}$
           </p>
