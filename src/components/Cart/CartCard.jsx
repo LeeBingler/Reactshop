c04dd9b1a0ea1b96c325ca44a287cef89db3d5a3
LeeBingler <binglerlee@gmail.com>
2023-07-15 18:03:13 +0200

    IU: style img card cart

--- src/components/Cart/CartCard.jsx
+++ src/components/Cart/CartCard.jsx
@@ -4,7 +4,7 @@ export default function CartCard({ item }) {
 
     return (
         <div className='mt-8 flex'>
-            <img className='h-20 px-4' src={item.image} alt={item.title} />
+            <img className='h-20 px-6' src={item.image} alt={item.title} />
             <div className='flex flex-col w-full max-w-4xl'>
                 <div className='flex justify-between'>
                     <h1 className='text-xl'> {item.title} </h1>
