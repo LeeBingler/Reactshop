d5b5296657766ef9f3f1e536961fa1fbc1c905a5
LeeBingler <binglerlee@gmail.com>
2023-07-16 11:40:21 +0200

    fix: nb of articles was false in cart Page

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -5,6 +5,15 @@ import CartCard from '../components/Cart/CartCard';
 export default function Cart() {
   const ItemCart = useCart();
   const totalPrice = useTotalPriceCart();
+  const nbItem = () => {
+    let nb = 0;
+
+    ItemCart.forEach(element => {
+      nb += element.number;
+    });
+
+    return nb;
+  };
 
     return (
       <section className='flex flex-col pt-20 pb-7 lg:pt-32 lg:pb-20 bg-gray-200 lg:flex-row lg:justify-around'>
@@ -22,7 +31,7 @@ export default function Cart() {
 
         <div className='flex flex-col items-center border border-black bg-white m-4 p-2 h-fit gap-4 min-w-[310px] lg:max-w-md lg:w-full lg:sticky lg:top-16'>
           <p className='text-right text-xl whitespace-nowrap'>
-            Total ({ItemCart.length} {ItemCart.length > 1 ? 'articles' : 'article'}): {totalPrice}$
+            Total ({nbItem()} {nbItem() > 1 ? 'articles' : 'article'}): {totalPrice}$
           </p>
           <button
           className="rounded border border-black p-2 m-2 bg-blue-400 text-white whitespace-nowrap">
