8a8dc1ede37d2eafa77af5d820f10e14b96a5140
LeeBingler <binglerlee@gmail.com>
2023-07-26 20:52:05 +0200

    fix: UI btn go back

--- src/components/GoBackBtn.jsx
+++ src/components/GoBackBtn.jsx
@@ -2,21 +2,22 @@ import { Link } from 'react-router-dom';
 import ScrollToTop from './ScrollToTop';
 
 export default function GoBackBtn() {
-  return (
-    <>
-      <button
-      aria-label='go to previous page'
-      className='text-blue-400 text-xl lg:text-2xl border border-black rounded px-[3px] '
-      >
-          <Link
-          to={-1}
-          onClick={() => {
-            window.scroll(0, 0);
-          }}>
-              Go Back
-          </Link>
-      </button>
-      <ScrollToTop />
-    </>
-  )
+    return (
+        <>
+            <button
+                aria-label='go to previous page'
+                className='text-blue-400 px-4 py-1 mt-2 text-xl lg:text-2xl border border-black rounded-md focus:ring-4 shadow-lg transform active:scale-75 transition-transform;'
+            >
+                <Link
+                    to={-1}
+                    onClick={() => {
+                        window.scroll(0, 0);
+                    }}
+                >
+                    Go Back
+                </Link>
+            </button>
+            <ScrollToTop />
+        </>
+    );
 }
