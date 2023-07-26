a9639d48856d76dd953236ac0bcd1d11bd10b542
LeeBingler <binglerlee@gmail.com>
2023-07-26 20:05:04 +0200

    fix: item rating card shop placement

--- src/components/Home/Shop/components/Card.jsx
+++ src/components/Home/Shop/components/Card.jsx
@@ -6,11 +6,11 @@ function Card({ item }) {
     const onClickHandler = useAddItemCart()
 
     return (
-        <div className='flex flex-col justify-center bg-white border border-black rounded m-4 p-2 py-10 lg:hover:shadow-2xl lg:hover:scale-[1.02] transition-transform ease-linear duration-200'>
-            <div className='flex flex-col justify-center items-center'>
-                <p className='text-right pb-5 w-full font-semibold mr-5 md:mr-7 lg:mr-12'>
+        <div className='flex flex-col justify-center relative bg-white border border-black rounded m-4 p-2 py-10 lg:hover:shadow-2xl lg:hover:scale-[1.02] transition-transform ease-linear duration-200'>
+            <p className='absolute top-2 text-right pb-5 w-[91%] font-semibold mr-5 md:mr-7 lg:mr-12'>
                     ⭐ {item.rating.rate}
-                </p>
+            </p>
+            <div className='flex flex-col justify-center items-center'>
                 <img className='max-h-64 w-60 px-4' src={item.image} alt={item.title} />
             </div>
             <h1 className='p-2 overflow-hidden text-ellipsis whitespace-nowrap text-gray-600 text-center
