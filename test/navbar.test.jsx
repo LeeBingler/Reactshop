20a56ea6874ea7817bb8cf328d8c3c9870e005d8
el <elidakirigo@gmail.com>
2023-08-03 15:03:00 +0300

    adding cart details

--- test/navbar.test.jsx
+++ test/navbar.test.jsx
@@ -4,19 +4,22 @@ import NavBar from '../src/components/Navbar/NavBar';
 import { beforeEach, describe, expect, test } from 'vitest';
 // Tests that the component renders without errors
 
+// --------------------    Navbar testing     -------------------------------- 
 describe('Navbar test', () => {
-	it('Navbar: render Component', () => {
-		const wrapper = render(<NavBar />);
-		expect(wrapper).toBeTruthy();
-	})
+	it('Navbar: render Component correctly', () => {
+		const NavbarComponent = render(<NavBar />);
 
+		expect(NavbarComponent).toBeTruthy();
+		expect(NavbarComponent).toMatchSnapshot();
+	})
+	// --------------------    Title testing     -------------------------------- 
 	it('test_display_title', () => {
 		render(<NavBar />);
 		const title = screen.getByText(/lReact Shop/i)
 		expect(title).toBeDefined()
 		expect(title).toBeInTheDocument()
 	})
-
+	// --------------------    Menu testing     -------------------------------- 
 	it('test_toggle_menu if url is correct', () => {
 		render(<NavBar />);
 		const home = screen.getByText(/home/i)
@@ -28,6 +31,7 @@ describe('Navbar test', () => {
 		expect(contact.href).toContain('/contact')
 	})
 
+	// --------------------    Search testing     --------------------------------
 	it('checks if all product id is available on search input', () => {
 
 	})
@@ -36,6 +40,8 @@ describe('Navbar test', () => {
 		fireEvent.click(document.getElementById('nextBtn'));
 		expect(screen.getByDisplayValue('test')).toBeInTheDocument();
 	})
+
+	// --------------------    Cart testing     -------------------------------- 
 	test("should not show the cart at the start", () => {
 		const cart = screen.getByText(/Content/i)
 		// expect().toBeUndefined()
@@ -48,18 +54,17 @@ describe('Navbar test', () => {
 	it('test remove button on all items in cart', () => {
 
 	})
+	
+	it('test the total from cart items', () => {
+		expect(' ').toBe(3)
+	})
 
 	it('test the go to cart button redirect', () => {
 
 	})
 	// beforeEach(() => {
 	// 	render(<Accordion title='Testing'><h4>Content</h4></Accordion>);
-	// });
-
-	test("should show title all the time", () => {
-
-		// expect(screen.getByText(/Testing/i)).toBeDefined()
-	})
+	// }); 
 
 	test("should not show the content at the start", () => {
 
