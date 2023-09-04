4f32082f765809f3db797d235c75122c1f89c090
LeeBingler <binglerlee@gmail.com>
2023-09-04 16:25:37 +0200

    feat: new style for the form

--- src/components/Contact/Blob.jsx
+++ src/components/Contact/Blob.jsx
@@ -2,7 +2,7 @@ export default function Blob() {
   return (
     <div className={`bg-[url('/blob.svg')] bg-no-repeat hidden md:flex  min-h-[300px] min-w-[300px]
         flex-col justify-center items-center`}>
-        <h1 className='text-blue-500 text-4xl font-subFont'> Don&apos;t be Afraid </h1>
+        <h1 className='text-blue-500 text-4xl font-subFont'> If there is a Probleme </h1>
         <p className='text-blue-500'>We will respond in less than 24h </p>
     </div>
   )
