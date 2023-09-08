d32df66e19f5ad6bee8dd044f2b27ae0698b0f94
LeeBingler <binglerlee@gmail.com>
2023-09-08 11:46:36 +0200

    feat: click on the image in the menu cart bring to the description of the item

--- src/components/Navbar/components/Menucart/components/CardCart.jsx
+++ src/components/Navbar/components/Menucart/components/CardCart.jsx
@@ -1,14 +1,22 @@
 import { useRemoveItemCart } from '../../../../Provider/CartProvider/Hook';
 import { PropTypes } from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function CardCart({ item }) {
     const onClickHandler = useRemoveItemCart();
 
     return (
         <div className='flex w-full h-24 justify-around items-center border-black border-b lg:text-xl lg:h-28'>
-            <div className='flex justify-center items-center h-[90%] w-24 lg:w-28 whitespace-nowrap'>
-                <img className='h-min max-h-full inline align-top float-none clear-none' src={item.image} alt={item.title} />
-            </div>
+            <Link
+                className='flex justify-center items-center h-[90%] w-24 lg:w-28 whitespace-nowrap'
+                to={`/product/${item.id}`}
+            >
+                <img
+                    className='h-min max-h-full inline align-top float-none clear-none'
+                    src={item.image}
+                    alt={item.title}
+                />
+            </Link>
 
             <div className='flex flex-col w-1/3 py-1'>
                 <h1 className='overflow-hidden text-ellipsis whitespace-nowrap'> {item.title} </h1>
@@ -17,14 +25,16 @@ function CardCart({ item }) {
             </div>
 
             <button
-            aria-label='Remove item'
-            className='rounded text-white bg-black p-1 h-10'
-            onClick={() => {onClickHandler(item, 0)}}
+                aria-label='Remove item'
+                className='rounded text-white bg-black p-1 h-10'
+                onClick={() => {
+                    onClickHandler(item, 0);
+                }}
             >
                 Remove
             </button>
         </div>
-    )
+    );
 }
 
 CardCart.propTypes = {
@@ -34,6 +44,6 @@ CardCart.propTypes = {
         price: PropTypes.number.isRequired,
         number: PropTypes.number.isRequired
     })
-}
+};
 
 export default CardCart;
