b913bf076191783b099ffadbdf9645472007c817
LeeBingler <binglerlee@gmail.com>
2023-09-08 11:31:13 +0200

    fix: menuCart cacontent are align

--- src/components/Navbar/components/Menucart/components/CardCart.jsx
+++ src/components/Navbar/components/Menucart/components/CardCart.jsx
@@ -5,13 +5,17 @@ function CardCart({ item }) {
     const onClickHandler = useRemoveItemCart();
 
     return (
-        <div className='flex w-full justify-around items-center border-black border-b lg:text-xl'>
-            <img className='h-20' src={item.image} alt={item.title} />
-            <div className='flex flex-col w-1/3'>
-                <h1 className='py-2 overflow-hidden text-ellipsis whitespace-nowrap'> {item.title} </h1>
-                <p> ${item.price} </p>
+        <div className='flex w-full h-24 justify-around items-center border-black border-b lg:text-xl lg:h-28'>
+            <div className='flex justify-center items-center h-[90%] w-24 lg:w-28 whitespace-nowrap'>
+                <img className='h-min max-h-full inline align-top float-none clear-none' src={item.image} alt={item.title} />
+            </div>
+
+            <div className='flex flex-col w-1/3 py-1'>
+                <h1 className='overflow-hidden text-ellipsis whitespace-nowrap'> {item.title} </h1>
+                <p className='font-medium py-1'> ${item.price} </p>
                 <p> x{item.number}</p>
             </div>
+
             <button
             aria-label='Remove item'
             className='rounded text-white bg-black p-1 h-10'
