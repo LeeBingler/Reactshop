39c0fbedbc061dd1ecf13bb7c82adbc18bea84bf
LeeBingler <binglerlee@gmail.com>
2023-09-10 12:36:20 +0200

    feat: add style descktop Product page

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -13,17 +13,17 @@ export default function Product() {
     if (useItems().length === 0) return <LoadingScreen />;
 
     return (
-        <section className='px-2 mt-28 flex flex-col items-center lg:flex-row lg:justify-around lg:items-start lg:mt-40 w-full'>
-            <div className='flex flex-col justify-center items-center border p-10 w-4/5'>
+        <section className='px-2 mt-28 flex flex-col items-center lg:flex-row lg:justify-around lg:items-start lg:mt-40 lg:px-14 w-full'>
+            <div className='flex flex-col justify-center items-center border p-10 w-4/5 max-w-sm xl:max-w-lg'>
                 <img className='h-min max-h-full' src={item.image} alt={item.title} />
             </div>
 
-            <div className='w-4/5 my-4 max-w-3xl lg:mb-0'>
-                <h1 className='py-5 font-medium text-2xl lg:text-2xl'>{item.title}</h1>
+            <div className='w-4/5 my-4 max-w-3xl lg:mb-0 lg:mt-0 lg:ml-5'>
+                <h1 className='py-5 font-medium text-2xl lg:text-2xl lg:mx-4 lg:pt-0'>{item.title}</h1>
                 <RatingStars rating={item.rating} />
-                <div className='border-t border-b border-gray-400 my-4 py-6 lg:mb-0 lg:pb-0'>
+                <div className='border-t border-b border-gray-400 my-4 py-6 lg:mb-0 lg:pb-0 lg:mx-4'>
                     <h2 className='mb-4 mt-1 text-xl font-medium'> About this item </h2>
-                    <p className='text-gray-600 text-justify md:text-xl md:p-4'>
+                    <p className='text-gray-600 text-justify 2xl:text-xl md:p-4'>
                         {item.description}
                     </p>
                 </div>
