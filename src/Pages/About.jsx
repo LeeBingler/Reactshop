add80d67bd4305188186b8ac9fe4ffd01841c2a4
LeeBingler <binglerlee@gmail.com>
2023-11-15 21:54:26 +0100

    style: dev notice is breakdown in accordion

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,4 +1,5 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { Accordion } from '../components/About/Accordion';
 
 export default function About() {
     return (
@@ -8,12 +9,14 @@ export default function About() {
             </div>
             <div className='px-10 flex flex-col items-center gap-7 ml-2 '>
                 <div className='flex flex-col justify-center content-center items-center w-full lg:w-[70%]'>
-                    <h2 className='text-3xl mb-14 mt-4 text-center lg:text-4xl'>We are the Reactshop!</h2>
+                    <h2 className='text-3xl mb-14 mt-4 text-center lg:text-4xl'>
+                        We are the Reactshop!
+                    </h2>
 
                     <div className='flex flex-col gap-7 w-full text-lg'>
                         <div className='flex flex-col items-end justify-between w-full'>
-                            <h2 className="title-aboutUs"> Our goal: </h2>
-                            <p className="text-aboutUs">
+                            <h2 className='title-aboutUs'> Our goal: </h2>
+                            <p className='text-aboutUs'>
                                 Our mission is to sell you what you really need at a fair cost. We
                                 take pride in offering you a wide range of high-quality products,
                                 including Men&apos;s and Women&apos;s Clothing, Jewelery and
@@ -21,8 +24,8 @@ export default function About() {
                             </p>
                         </div>
                         <div>
-                            <h2 className="title-aboutUs">Our story :</h2>
-                            <p className="text-aboutUs">
+                            <h2 className='title-aboutUs'>Our story :</h2>
+                            <p className='text-aboutUs'>
                                 Our company was born out of an ideal futur for all the customer.
                                 Since then, we&apos;ve evolved to become a major player in the
                                 industry, consistently focusing on our core values of sincerity,
@@ -30,8 +33,8 @@ export default function About() {
                             </p>
                         </div>
                         <div className='flex flex-col items-end justify-between '>
-                            <h2 className="title-aboutUs"> Our values :</h2>
-                            <p className="text-aboutUs">
+                            <h2 className='title-aboutUs'> Our values :</h2>
+                            <p className='text-aboutUs'>
                                 At Reactshop, we believe in sincerity, sharing, communication. Our
                                 dedicated team works tirelessly to provide our customers with an
                                 exceptional shopping experience. Our commitment to quality,
@@ -40,8 +43,8 @@ export default function About() {
                             </p>
                         </div>
                         <div>
-                            <h2 className="title-aboutUs"> Thank you: </h2>
-                            <p className="text-aboutUs">
+                            <h2 className='title-aboutUs'> Thank you: </h2>
+                            <p className='text-aboutUs'>
                                 At Reactshop, we are determined to give you the best experience
                                 imaginable and to create an unforgettable experience. We are excited
                                 to have you with us and to assist you with your online shopping.
@@ -52,19 +55,25 @@ export default function About() {
                         </div>
                     </div>
 
-                    <h2 className='font-aboutTitle text-2xl underline mb-4 mt-8'> - The Reactshop Team</h2>
-                    <Link
-                    className="btn-addToCart text-2xl"
-                    to='/contact'
-                    > Contact Us </Link>
+                    <h2 className='font-aboutTitle text-2xl underline mb-4 mt-8'>
+                        {' '}
+                        - The Reactshop Team
+                    </h2>
+                    <Link className='btn-addToCart text-2xl' to='/contact'>
+                        {' '}
+                        Contact Us{' '}
+                    </Link>
                 </div>
 
                 <hr className='border-b border-black w-[70%] m-auto h-[1px]' />
 
-                <div className='flex flex-col gap-10 max-w-7xl'>
-                    <div className='container-about'>
-                        <h2 className='subtitle-about'>Dev Notice :</h2>
-                        <div className='text-lg lg:text-xl'>
+                <div className='flex flex-col mb-10 gap-5'>
+                    <Accordion
+                        title='Development notice'
+                        classTitle={'text-lg lg:text-xl py-2'}
+                        classContainer={'bg-gray-300'}
+                    >
+                        <div className='text-lg lg:text-xl flex flex-col gap-3 justify-center md:h-full'>
                             <p>
                                 The ReactShop is a project made by{' '}
                                 <a
@@ -84,52 +93,74 @@ export default function About() {
                                 i want in the App.
                             </p>
                         </div>
-                    </div>
-
-                    <div className='container-about'>
-                        <h2 className='subtitle-about'>Source Code: </h2>
-                        <a
-                            className='text-blue-500 underline hover:text-purple-500 lg:text-lg'
-                            href='https://github.com/LeeBingler/Reactshop'
-                        >
-                            Reactshop
-                        </a>
-                    </div>
-
-                    <div className='container-about'>
-                        <h2 className='subtitle-about'>
-                            Documentation librairie and framework used :
-                        </h2>
-                        <ul className='px-7 lg:text-lg lg:px-10'>
+                    </Accordion>
+                    <Accordion
+                        title='Source Code'
+                        classTitle={'text-lg lg:text-xl py-2'}
+                        classContainer={'bg-gray-300'}
+                    >
+                        <div className='text-center text-lg lg:text-xl flex flex-col gap-3 justify-center md:h-full'>
+                            <a
+                                className='text-blue-500 underline hover:text-purple-500 lg:text-lg '
+                                href='https://github.com/LeeBingler/Reactshop'
+                            >
+                                Reactshop
+                            </a>
+                            <p>
+                                ReactShop is an impressive e-commerce website built with React.JS
+                                and Tailwind. It allows users to browse through various products and
+                                make purchases. The project's code is licensed under the MIT
+                                License, providing flexibility for usage and distribution.
+                            </p>
+                        </div>
+                    </Accordion>
+                    <Accordion
+                        title='Documentation librairie and framework used'
+                        classTitle={'text-lg lg:text-xl py-2'}
+                        classContainer={'bg-gray-300'}
+                    >
+                        <ul className='lg:text-lg h-full text-center flex flex-col gap-5 justify-center'>
                             <li>
-                                -{' '}
-                                <a
-                                    className='text-blue-500 underline hover:text-purple-500'
-                                    href='https://tailwindcss.com/'
-                                >
-                                    Tailwind
-                                </a>
+                                <p>
+                                    -{' '}
+                                    <a
+                                        className='text-blue-500 underline hover:text-purple-500'
+                                        href='https://tailwindcss.com/'
+                                    >
+                                        Tailwind
+                                    </a>{' '}
+                                    : A utility-first CSS framework that streamlines styling and
+                                    allows for the creation of clean and responsive designs.
+                                </p>
                             </li>
                             <li>
-                                -{' '}
-                                <a
-                                    className='text-blue-500 underline hover:text-purple-500'
-                                    href='https://react.dev/'
-                                >
-                                    React.js
-                                </a>
+                                <p>
+                                    -{' '}
+                                    <a
+                                        className='text-blue-500 underline hover:text-purple-500'
+                                        href='https://react.dev/'
+                                    >
+                                        React.js
+                                    </a>{' '}
+                                    : A popular JavaScript library for building user interfaces,
+                                    enabling fast and dynamic updates to the website.
+                                </p>
                             </li>
                             <li>
-                                -{' '}
-                                <a
-                                    className='text-blue-500 underline hover:text-purple-500'
-                                    href='https://vitest.dev/'
-                                >
-                                    Vitest
-                                </a>
+                                <p>
+                                    -{' '}
+                                    <a
+                                        className='text-blue-500 underline hover:text-purple-500'
+                                        href='https://vitest.dev/'
+                                    >
+                                        Vitest
+                                    </a>{' '}
+                                    : Vitest is a next generation testing framework powered by Vite
+                                    and which enables a simple test setup and management experience.
+                                </p>
                             </li>
                         </ul>
-                    </div>
+                    </Accordion>
                 </div>
             </div>
         </section>
