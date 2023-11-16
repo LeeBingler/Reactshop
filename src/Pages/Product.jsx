d28782cfb454435eb4f5868668858282e53e0a33
LeeBingler <binglerlee@gmail.com>
2023-11-16 17:09:19 +0100

    fix: change tag ProductPage

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -15,20 +15,20 @@ export default function Product() {
 
     return (
         <section className='px-2 mt-28 flex flex-col items-center lg:flex-row lg:justify-around lg:items-start lg:mt-40 lg:px-14 w-full'>
-            <div className='flex flex-col justify-center items-center border p-10 w-4/5 max-w-sm xl:max-w-lg'>
+            <article className='flex flex-col justify-center items-center border p-10 w-4/5 max-w-sm xl:max-w-lg'>
                 <img className='h-min max-h-full' src={item.image} alt={item.title} />
-            </div>
+            </article>
 
-            <div className='w-4/5 my-4 max-w-3xl lg:mb-0 lg:mt-0 lg:ml-5'>
-                <h1 className='py-5 font-medium text-2xl lg:text-2xl lg:mx-4 lg:pt-0'>{item.title}</h1>
+            <article className='w-4/5 my-4 max-w-3xl lg:mb-0 lg:mt-0 lg:ml-5'>
+                <h2 className='py-5 font-medium text-2xl lg:text-2xl lg:mx-4 lg:pt-0'>{item.title}</h2>
                 <RatingStars rating={item.rating} />
                 <DescriptionItem description={item.description} />
-            </div>
+            </article>
 
-            <div className='flex flex-col w-4/5 justify-center items-center'>
+            <article className='flex flex-col w-4/5 justify-center items-center'>
                 <CTAsection item={item} />
                 <GoBackBtn  className='w-32' />
-            </div>
+            </article>
         </section>
     );
 }
