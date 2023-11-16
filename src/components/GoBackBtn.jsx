8c5d6b824bf8a00a1e0a67ea787e331201776399
LeeBingler <binglerlee@gmail.com>
2023-11-16 15:47:40 +0100

    feat: the checkout button loading state have animation

--- src/components/GoBackBtn.jsx
+++ src/components/GoBackBtn.jsx
@@ -6,7 +6,7 @@ function GoBackBtn({ className }) {
         <>
             <button
                 aria-label='go to previous page'
-                className={`${className} text-blue-400 px-4 py-1 mt-2 text-xl lg:text-2xl border border-black rounded-md focus:ring-4 transform active:scale-75 transition-transform hover:bg-gray-200`}
+                className={`${className || ''} text-blue-400 px-4 py-1 mt-2 text-xl lg:text-2xl border border-black rounded-md focus:ring-4 transform active:scale-75 transition-transform hover:bg-gray-200`}
             >
                 <Link
                     to={-1}
