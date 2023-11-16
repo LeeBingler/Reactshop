2b95b34a73fb0b85ee5cf75372c7032c23021bd6
LeeBingler <binglerlee@gmail.com>
2023-11-16 16:58:44 +0100

    fix: style title Description Item

--- src/components/Product/DescriptionItem.jsx
+++ src/components/Product/DescriptionItem.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function DescriptionItem({description}) {
     return (
         <div className='border-t border-b border-gray-400 my-4 py-6 lg:mb-0 lg:pb-0 lg:mx-4'>
-            <h2 className='mb-4 mt-1 text-xl font-medium'> About this item </h2>
+            <h2 className='mb-4 mt-1 text-xl font-medium md:pl-4'> About this item </h2>
             <p className='text-gray-600 text-justify 2xl:text-xl md:p-4'>{description}</p>
         </div>
     );
