bcc3de0490485a898f41cab3195add6df12cf86b
LeeBingler <binglerlee@gmail.com>
2023-11-17 15:13:58 +0100

    refactor: change rating star compoent

--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -5,6 +5,7 @@ import GoBackBtn from '../components/GoBackBtn';
 import RatingStars from '../components/Product/RatingStars';
 import CTAsection from '../components/Product/CTAsection';
 import DescriptionItem from '../components/Product/DescriptionItem';
+import RatingItem from '../components/Product/RatingItem';
 
 export default function Product() {
     const idProduct = Number(useParams().id);
@@ -14,21 +15,29 @@ export default function Product() {
     if (useItems().length === 0) return <LoadingScreen />;
 
     return (
-        <section className='px-2 mt-28 flex flex-col items-center lg:flex-row lg:justify-around lg:items-start lg:mt-40 lg:px-14 w-full'>
-            <article className='flex flex-col justify-center items-center border p-10 w-4/5 max-w-sm xl:max-w-lg'>
-                <img className='h-min max-h-full' src={item.image} alt={item.title} />
-            </article>
+        <>
+            <section className='px-2 mt-28 flex flex-col items-center lg:flex-row lg:justify-around lg:items-start lg:mt-40 lg:px-14 w-full'>
+                <article className='flex flex-col justify-center items-center border p-10 w-4/5 max-w-sm xl:max-w-lg'>
+                    <img className='h-min max-h-full' src={item.image} alt={item.title} />
+                </article>
 
-            <article className='w-4/5 my-4 max-w-3xl lg:mb-0 lg:mt-0 lg:ml-5'>
-                <h2 className='py-5 font-medium text-2xl lg:text-2xl lg:mx-4 lg:pt-0'>{item.title}</h2>
+                <article className='w-4/5 my-4 max-w-3xl lg:mb-0 lg:mt-0 lg:ml-5'>
+                    <h2 className='py-5 font-medium text-2xl lg:text-2xl lg:mx-4 lg:pt-0'>
+                        {item.title}
+                    </h2>
+                    <RatingItem rating={item.rating}/>
+                    <DescriptionItem description={item.description} />
+                </article>
+
+                <article className='flex flex-col w-4/5 justify-center items-center'>
+                    <CTAsection item={item} />
+                    <GoBackBtn className='w-32' />
+                </article>
+            </section>
+            <section>
+                <h2>Reviews</h2>
                 <RatingStars rating={item.rating} />
-                <DescriptionItem description={item.description} />
-            </article>
-
-            <article className='flex flex-col w-4/5 justify-center items-center'>
-                <CTAsection item={item} />
-                <GoBackBtn  className='w-32' />
-            </article>
-        </section>
+            </section>
+        </>
     );
 }
--- src/components/Product/RatingStars.jsx
+++ src/components/Product/RatingStars.jsx
@@ -2,19 +2,18 @@ import { PropTypes } from 'prop-types';
 import { HalfStar, EmptyStar, FullStar } from './Stars';
 
 function RatingStars({ rating }) {
-    const {rate, count} = rating;
+    const { rate, count } = rating;
     const arrayStar = setArrayStar();
 
-    function setArrayStar () {
+    function setArrayStar() {
         let arrayStar = [];
         let cpyRate = Number(rate);
-
 
         for (; cpyRate >= 1; cpyRate--) {
             arrayStar.push(<FullStar key={cpyRate * 78945} />);
         }
 
-        if (arrayStar.length != 5 && (cpyRate >= 0.5 && cpyRate < 0.9)) {
+        if (arrayStar.length != 5 && cpyRate >= 0.5 && cpyRate < 0.9) {
             arrayStar.push(<HalfStar key={cpyRate * 10 * 4854} />);
         }
 
@@ -25,16 +24,7 @@ function RatingStars({ rating }) {
         return arrayStar;
     }
 
-    return (
-        <div className='flex py-3 gap-6'>
-            <p className='font-semibold text-yellow-400 md:ml-4'>
-                {
-                    arrayStar
-                }
-            </p>
-            <p className='text-blue-800'> {count} ratings </p>
-        </div>
-    );
+    return <p className='font-semibold text-yellow-400 md:ml-4'>{arrayStar}</p>;
 }
 
 RatingStars.propTypes = {
@@ -42,6 +32,6 @@ RatingStars.propTypes = {
         rate: PropTypes.number,
         count: PropTypes.number
     }).isRequired
-}
+};
 
 export default RatingStars;
