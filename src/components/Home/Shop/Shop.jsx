1aeb3cc5b9a0cbd9480d0ab4b603f281eed1c8be
LeeBingler <binglerlee@gmail.com>
2023-11-18 16:27:11 +0100

    fix: change style filter active

--- src/components/Home/Shop/Shop.jsx
+++ src/components/Home/Shop/Shop.jsx
@@ -1,29 +1,29 @@
-import { useReducer } from 'react'
-import { useItems } from '../../Provider/ItemsProvider/Hook'
+import { useReducer } from 'react';
+import { useItems } from '../../Provider/ItemsProvider/Hook';
 import Card from './components/Card';
 import FilterShop from './components/FilterShop';
 import { ACTION } from './components/Action';
 
 function reducerFilterShop(state, action) {
-    switch(action.filterType) {
+    switch (action.filterType) {
         case ACTION.ALL:
             return action.Items;
         case ACTION.WOMEN:
             return action.Items.filter((item) => {
                 return item.category === ACTION.WOMEN;
-            })
+            });
         case ACTION.MEN:
             return action.Items.filter((item) => {
                 return item.category === ACTION.MEN;
-            })
+            });
         case ACTION.JEWELERY:
-                return action.Items.filter((item) => {
-                    return item.category === ACTION.JEWELERY;
-            })
+            return action.Items.filter((item) => {
+                return item.category === ACTION.JEWELERY;
+            });
         case ACTION.ELECTRONICS:
             return action.Items.filter((item) => {
-                    return item.category === ACTION.ELECTRONICS;
-            })
+                return item.category === ACTION.ELECTRONICS;
+            });
         default:
             return state;
     }
@@ -34,13 +34,15 @@ export default function Shop() {
 
     return (
         <section className=' max-w-full flex flex-col md:flex-row'>
-            <FilterShop filterFunction={dispatch}/>
-            <div className='grid grid-cols-2 gap-y-2
-                xl:grid-cols-3 md:border-l md:border-black md:mt-2'>
+            <FilterShop filterFunction={dispatch} />
+            <div
+                className='grid grid-cols-2 gap-y-2
+                xl:grid-cols-3 md:border-l md:border-black md:mt-2'
+            >
                 {itemsDisplay.map((item) => {
-                    return <Card key={item.id} item={item} />
+                    return <Card key={item.id} item={item} />;
                 })}
             </div>
         </section>
-    )
+    );
 }
--- src/components/Home/Shop/components/FilterShop.jsx
+++ src/components/Home/Shop/components/FilterShop.jsx
@@ -1,56 +1,74 @@
 import { ACTION } from './Action';
 import { useItems } from '../../../Provider/ItemsProvider/Hook';
 import { PropTypes } from 'prop-types';
+import { useState } from 'react';
 
 function FilterShop({ filterFunction }) {
+    const [whichIsActive, setWhichIsActive] = useState(0);
     const AllItems = useItems();
 
     return (
         <section
-        className='flex max-w-full justify-center border-black bg-white border-b border-t mt-4 py-2 sticky top-[4.7rem] z-10
+            className='flex max-w-full justify-center border-black bg-white border-b border-t mt-4 py-2 sticky top-[4.7rem] z-10
                 md:border-0 md:mt-0 md:h-16 md:w-[1000px] md:flex-wrap md:justify-normal md:pr-4 md:top-20 md:max-w-xs
-                lg:w-1/2'>
+                lg:w-1/2'
+        >
             <button
-            aria-label='filter All'
-            className='btn-filter w-16'
-            onClick={() => filterFunction({ filterType: ACTION.ALL, Items: AllItems })}
+                aria-label='filter All'
+                className={`w-16 ${whichIsActive === 0 ? 'filter-active' : 'btn-filter'}`}
+                onClick={() => {
+                    filterFunction({ filterType: ACTION.ALL, Items: AllItems });
+                    if (whichIsActive != 0) setWhichIsActive(0);
+                }}
             >
                 All
             </button>
             <button
-            aria-label="filter Women's Clothing"
-            className='btn-filter'
-            onClick={() => filterFunction({ filterType: ACTION.WOMEN, Items: AllItems })}
+                aria-label="filter Women's Clothing"
+                className={`${whichIsActive === 1 ? 'filter-active' : 'btn-filter'}`}
+                onClick={() => {
+                    filterFunction({ filterType: ACTION.WOMEN, Items: AllItems });
+                    if (whichIsActive != 1) setWhichIsActive(1);
+                }}
             >
                 Women&apos;s Clothing
             </button>
             <button
-            aria-label="filter Men's Clothing"
-            className='btn-filter'
-            onClick={() => filterFunction({ filterType: ACTION.MEN, Items: AllItems })}
+                aria-label="filter Men's Clothing"
+                className={`${whichIsActive === 2 ? 'filter-active' : 'btn-filter'}`}
+                onClick={() => {
+                    filterFunction({ filterType: ACTION.MEN, Items: AllItems });
+                    if (whichIsActive != 2) setWhichIsActive(2);
+                }}
             >
                 Men&apos;s Clothing
             </button>
             <button
-            aria-label="filter Jewelery"
-            className='btn-filter'
-            onClick={() => filterFunction({ filterType: ACTION.JEWELERY, Items: AllItems })}
+                aria-label='filter Jewelery'
+                className={`${whichIsActive === 3 ? 'filter-active' : 'btn-filter'}`}
+                onClick={() => {
+                    filterFunction({ filterType: ACTION.JEWELERY, Items: AllItems });
+                    if (whichIsActive != 3) setWhichIsActive(3);
+                }}
             >
                 Jewelery
             </button>
             <button
-            aria-label="filter Electronics"
-            className='btn-filter'
-            onClick={() => filterFunction({ filterType: ACTION.ELECTRONICS, Items: AllItems })}
+                aria-label='filter Electronics'
+                className={`${whichIsActive === 4 ? 'filter-active' : 'btn-filter'}`}
+                onClick={() => {
+                    filterFunction({ filterType: ACTION.ELECTRONICS, Items: AllItems });
+                    if (whichIsActive != 4) setWhichIsActive(4);
+                }}
             >
                 Electronics
             </button>
         </section>
-    )
+    );
 }
 
 FilterShop.propTypes = {
     filterFunction: PropTypes.func.isRequired
-}
+};
 
 export default FilterShop;
